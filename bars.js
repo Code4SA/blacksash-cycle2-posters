d97a1eb040c0b93199be5bbbca5f13faa743386e
Adi Eyal <adi@code4sa.org>
2015-08-18 22:15:31 +0200

    Added horizontal bar chart

--- bars.js
+++ bars.js
@@ -1,4 +1,4 @@
-function HorizontalBars(node, data, ctx) {
+function HorizontalStackedBars(node, data, ctx) {
     this.node = node;
     this.data = data;
     this.width = ctx.width || 420;
@@ -9,7 +9,7 @@ function HorizontalBars(node, data, ctx) {
     this.draw();
 }
 
-HorizontalBars.prototype = {
+HorizontalStackedBars.prototype = {
     draw : function() {
         var me = this;
 
@@ -53,4 +53,90 @@ HorizontalBars.prototype = {
     },
 }
 
+function HorizontalBars(node, data, ctx) {
+    this.node = node;
+    this.data = data;
+    this.height = ctx.height || 10;
+    this.height1 = ctx.height1 || 5;
+    this.height2 = this.height - this.height1;
+    this.heights = [this.height1, this.height - this.height1];
+    this.width = ctx.width || 100;
+    this.border_width = ctx.border_width || 1;
+    this.class_prefix = ctx.class_prefix || "bar";
 
+    var foo = function(val) {
+        return text_length(node, val);
+    }
+    this.label_width = d3.max(this.data.map(foo));
+    this.label_space = ctx.label_space || 10;
+    this.flip = ctx.flip || false;
+
+    this.draw();
+}
+
+
+HorizontalBars.prototype = {
+    draw : function() {
+        var me = this;
+        var max_data = d3.max(this.data);
+        var x = d3.scale.linear()
+            .domain([0, d3.max(this.data)])
+            .range([0, this.width - this.label_space - this.label_width]);
+
+
+        var y = d3.scale.linear()
+            .domain([0, 1])
+            .range([0, this.height1]);
+
+        this.node 
+            .attr("width", this.width)
+            .attr("height", this.height);
+
+        var bar = this.node.selectAll("g")
+            .data(this.data)
+            .enter()
+            .append("g")
+                .attr("class", function(el, i) {
+                    return me.class_prefix + " " + me.class_prefix + "_" + i;
+                })
+                .attr("transform", function(el, i) {
+                    if (me.flip) {
+                        return "translate(" + (me.width - x(el)) + ", " + y(i) + ")"
+                    } else {
+                        return "translate(0, " + y(i) + ")"
+                    }
+                })
+
+        bar.append("rect")
+            .attr("width", function(el, i) { return x(el); })
+            .attr("height", function(el, i) {
+                return me.heights[i];
+            });
+
+        bar.append("text")
+            .attr("x", function(d, i) {
+                if (me.flip) {
+                    console.log(d);
+                    console.log(x(d));
+                    console.log(me.width - x(d) - me.label_space - me.label_width);
+                    return -me.label_space;
+                } else {
+                    return x(d) + me.label_space + me.label_width;
+                }
+            })
+            .attr("y", function(d, i) {
+                return me.heights[i] / 2;
+            })
+            .attr("dy", "+.35em")
+            .text(function(d) { return d; });
+
+        
+    }    
+}
+
+var text_length = function(node, val) {
+    var n = node.append("text").text(val);
+    var width = n.node().getComputedTextLength();
+    n.remove();
+    return width;
+}
