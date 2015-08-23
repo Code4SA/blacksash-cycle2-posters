792194b71154763ca2125e6c5e4301669d3b18c3
Adi Eyal <adi@code4sa.org>
2015-08-23 20:35:14 +0200

    Added participants numbers

--- poster.js
+++ poster.js
@@ -1,6 +1,18 @@
 var data = {
     name : 'XYZ Office',
     location : 'Mbombela',
+    participants : {
+        "2015" : {
+            total : 100,
+            female : 75,
+            male : 25
+        },
+        "2014" : {
+            total : 56,
+            female : 36,
+            male : 20
+        },
+    },
     q1 : [
         { women : [10, 100], men : [30, 80] },
         { women : [20, 90], men : [16, 15] },
@@ -248,6 +260,12 @@ var place_type3_question = function(node, data, ctx) {
 var place_elements = function(node) {
     node.selectAll('#organisation-name tspan').text(data['name']);
     node.selectAll('#office-location').text(data['location']);
+    node.selectAll('#participants-2015 .total').text(data['participants']['2015']['total']);
+    node.selectAll('#participants-2015 .female').text(data['participants']['2015']['female']);
+    node.selectAll('#participants-2015 .male').text(data['participants']['2015']['male']);
+    node.selectAll('#participants-2014 .total').text(data['participants']['2014']['total']);
+    node.selectAll('#participants-2014 .female').text(data['participants']['2014']['female']);
+    node.selectAll('#participants-2014 .male').text(data['participants']['2014']['male']);
 
     place_type3_question(node.select('#q1'), data.q1);
     place_type1_question(node.select('#q2'), data.q2);
@@ -256,6 +274,7 @@ var place_elements = function(node) {
     place_type1_question(node.select('#q5'), data.q5);
     place_type1_question(node.select('#q6'), data.q6);
     place_type3_question(node.select('#q7'), data.q7, { height:48, height1: 32, width:150 });
+    place_type3_question(node.select('#q8'), data.q8, { height:48, height1: 32, width:150 });
     place_type2_question(node.select('#q9'), data.q9);
     place_type2_question(node.select('#q10'), data.q10);
     place_type2_question(node.select('#q11'), data.q11);
